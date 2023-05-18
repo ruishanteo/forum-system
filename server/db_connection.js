9f8e1d8a1210215f392b15b0a64fe5915d296572
ruishanteo <ruishan.teo03@gmail.com>
2023-05-18 23:28:15 +0800

    added db

---
+++ server/db_connection.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
