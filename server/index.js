96d145f6d535053552525ceb544d379955614867
ruishanteo <ruishan.teo03@gmail.com>
2023-05-19 20:36:43 +0800

    migrated remaining api endpoints to use mysql

--- server/index.js
+++ server/index.js
@@ -10,11 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const users = [];
-const threadList = [];
-
-const generateID = () => Math.random().toString(36).substring(2, 10);
-
 app.post("/api/register", async (req, res) => {
   const user = req.body.username;
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -56,10 +51,12 @@ app.post("/api/login", (req, res) => {
       });
     } else {
       const hashedPassword = result[0].password;
+      const id = result[0].userId;
 
       if (await bcrypt.compare(password, hashedPassword)) {
         return res.json({
           message: `Welcome, ${user}!`,
+          id: id,
         });
       } else {
         return res.json({
@@ -71,60 +68,120 @@ app.post("/api/login", (req, res) => {
 });
 
 app.post("/api/create/thread", async (req, res) => {
-  const { thread, userId } = req.body;
-  const threadId = generateID();
+  const author = req.body.userId;
+  const title = req.body.title;
+  const text = req.body.thread;
 
-  threadList.unshift({
-    id: threadId,
-    title: thread,
-    userId,
-    replies: [],
-    likes: [],
-  });
+  const sqlInsert = "INSERT INTO threads VALUES (0, ?, ?, ?, 0, 0)";
+  const insert_query = mysql.format(sqlInsert, [author, title, text]);
 
-  res.json({
-    message: "Thread created successfully!",
-    threads: threadList,
-  });
-});
-
-app.post("/api/thread/like", (req, res) => {
-  const { threadId, userId } = req.body;
-  const result = threadList.filter((thread) => thread.id === threadId);
-  const threadLikes = result[0].likes;
-  const authenticateReaction = threadLikes.filter((user) => user === userId);
-  if (authenticateReaction.length === 0) {
-    threadLikes.push(userId);
+  await db.connection.query(insert_query, (err, result) => {
+    if (err) throw err;
     return res.json({
-      message: "You've reacted to the post!",
+      message: "Thread created successfully!",
     });
-  }
-  res.json({
-    error_message: "You can only react once!",
   });
 });
 
-app.post("/api/thread/replies", (req, res) => {
-  const { id } = req.body;
-  const result = threadList.filter((thread) => thread.id === id);
-  res.json({
-    replies: result[0].replies,
-    title: result[0].title,
+app.post("/api/thread/like", async (req, res) => {
+  const threadId = req.body.threadId;
+  const userId = req.body.userId;
+
+  const sqlSearch = "SELECT * FROM threads WHERE threadId = ?";
+  const search_query = mysql.format(sqlSearch, [threadId]);
+
+  await db.connection.query(search_query, async (err, result) => {
+    if (err) throw err;
+    if (result.length != 0) {
+      const sqlUpdate = "UPDATE threads SET likesCount = ? WHERE threadId = ?";
+      const update_query = mysql.format(sqlUpdate, [
+        result[0].likesCount + 1,
+        threadId,
+      ]);
+      await db.connection.query(update_query, async (err, result) => {
+        if (err) {
+          throw err;
+        } else {
+          return res.json({
+            message: "Like successful!",
+          });
+        }
+      });
+    }
+  });
+});
+
+app.post("/api/thread/replies", async (req, res) => {
+  const threadId = req.body.id;
+
+  const sqlSearch = "SELECT * FROM replies WHERE threadId = ?";
+  const search_query = mysql.format(sqlSearch, [threadId]);
+
+  await db.connection.query(search_query, async (err, repliesResult) => {
+    if (err) throw err;
+    const sqlSearchTitle = "SELECT * FROM threads WHERE threadId = ?";
+    const search_title_query = mysql.format(sqlSearchTitle, [threadId]);
+
+    await db.connection.query(search_title_query, async (err, threadResult) => {
+      if (err) throw err;
+
+      res.json({
+        replies: repliesResult,
+        title: threadResult[0].title,
+      });
+    });
   });
 });
 
 app.post("/api/create/reply", async (req, res) => {
-  const { id, userId, reply } = req.body;
-  const result = threadList.filter((thread) => thread.id === id);
-  const user = users.filter((user) => user.id === userId);
-  result[0].replies.unshift({
-    userId: user[0].id,
-    name: user[0].username,
-    text: reply,
-  });
+  const threadId = req.body.id;
+  const author = req.body.userId;
+  const text = req.body.reply;
 
-  res.json({
-    message: "Response added successfully!",
+  const sqlSearchUser = "SELECT * FROM users WHERE userId = ?";
+  const search_user_query = mysql.format(sqlSearchUser, [author]);
+
+  await db.connection.query(search_user_query, async (err, userResult) => {
+    if (err) throw err;
+    if (userResult.length == 0) {
+      res.json({
+        error_message: "Invalid user!",
+      });
+    }
+    const sqlInsert = "INSERT INTO replies VALUES (0, ?, ?, ?, ?)";
+    const insert_query = mysql.format(sqlInsert, [
+      threadId,
+      author,
+      userResult[0].user,
+      text,
+    ]);
+
+    await db.connection.query(insert_query, (err, result) => {
+      if (err) throw err;
+      res.json({
+        message: "Response added successfully!",
+      });
+    });
+
+    const sqlSearch = "SELECT * FROM threads WHERE threadId = ?";
+    const search_query = mysql.format(sqlSearch, [threadId]);
+
+    await db.connection.query(search_query, async (err, result) => {
+      if (err) throw err;
+      if (result.length != 0) {
+        const sqlUpdate =
+          "UPDATE threads SET repliesCount = ? WHERE threadId = ?";
+        const update_query = mysql.format(sqlUpdate, [
+          result[0].repliesCount + 1,
+          threadId,
+        ]);
+        await db.connection.query(update_query, async (err, result) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    });
   });
 });
 
@@ -134,9 +191,14 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.get("/api/all/threads", (req, res) => {
-  res.json({
-    threads: threadList,
+app.get("/api/all/threads", async (req, res) => {
+  const sqlSearch = "SELECT * FROM threads";
+
+  await db.connection.query(sqlSearch, async (err, result) => {
+    if (err) throw err;
+    res.json({
+      threads: result,
+    });
   });
 });
 
