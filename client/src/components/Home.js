e790c33914f5b4845bef1ad0fa389312fde75782
ruishanteo <ruishan.teo03@gmail.com>
2023-05-20 08:49:18 +0800

    formatted home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 import Likes from "../utils/Likes.js";
 import Comments from "../utils/Comments.js";
@@ -104,30 +113,46 @@ const Home = () => {
         </Button>
       </Box>
 
-      <Box align="left" sx={{ ml: 8 }}>
-        {threadList.map((thread) => (
-          <Box key={thread.threadId}>
-            <Typography>{thread.title}</Typography>
-            <Typography>{thread.text}</Typography>
-            <Box
-              className="react__container"
-              maxWidth="5vw"
-              display="flex"
-              flexDirection="row"
-            >
-              <Likes
-                numberOfLikes={thread.likesCount}
-                threadId={thread.threadId}
-              />
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h2">Posts</Typography>
+        <Box sx={{ mx: 2, px: 20 }}>
+          <List>
+            {threadList.map((thread) => (
+              <Box key={thread.threadId}>
+                <Grid container spacing={3} sx={{ mt: 4 }}>
+                  <ListItem>
+                    <Grid item xs={8}>
+                      <Typography variant={"h5"}>{thread.title}</Typography>
+                      <Typography variant={"body1"}>{thread.text}</Typography>
+                    </Grid>
+                    <Grid item xs>
+                      <Box align="right" mr={10}>
+                        <Box
+                          className="react__container"
+                          maxWidth="5vw"
+                          display="flex"
+                          flexDirection="row"
+                        >
+                          <Likes
+                            numberOfLikes={thread.likesCount}
+                            threadId={thread.threadId}
+                          />
 
-              <Comments
-                numberOfComments={thread.repliesCount}
-                threadId={thread.threadId}
-                title={thread.title}
-              />
-            </Box>
-          </Box>
-        ))}
+                          <Comments
+                            numberOfComments={thread.repliesCount}
+                            threadId={thread.threadId}
+                            title={thread.title}
+                          />
+                        </Box>
+                      </Box>
+                    </Grid>
+                  </ListItem>
+                </Grid>
+                <Divider />
+              </Box>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
